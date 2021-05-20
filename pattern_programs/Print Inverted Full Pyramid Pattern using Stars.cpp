a83f78e76c94645dbea811a225d368db558cf5bd
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-20 22:55:16 +0530

    Create Print Inverted Full Pyramid Pattern using Stars.cpp

---
+++ pattern_programs/Print Inverted Full Pyramid Pattern using Stars.cpp
@@ -0,0 +1,19 @@
+// C++ Print Inverted Full Pyramid Pattern using Stars
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, space, j;
+    for(i=1; i<=6; i++)
+    {
+        for(space=1; space<i; space++)
+            cout<<" ";
+        for(j=i; j<=6; j++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
