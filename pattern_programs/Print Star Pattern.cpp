296e5d4ef53ecb80e56a607b796ce22627d87c5c
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-20 23:05:59 +0530

    Create Print Star Pattern.cpp

---
+++ pattern_programs/Print Star Pattern.cpp
@@ -0,0 +1,16 @@
+// C++ Print Star Pattern
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j, k=1;
+    for(i=0; i<6; i++)
+    {
+        for(j=0; j<k; j++)
+            cout<<"* ";
+        k=k+2;
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
