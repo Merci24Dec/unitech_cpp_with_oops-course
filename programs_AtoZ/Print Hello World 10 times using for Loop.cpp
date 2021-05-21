e8890ec93a97c4fdd70ac7c2027b49b5cb1b301f
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-21 19:06:28 +0530

    Create Print Hello World 10 times using for Loop.cpp

---
+++ programs_AtoZ/Print Hello World 10 times using for Loop.cpp
@@ -0,0 +1,11 @@
+// C++ Print Hello World 10 times using for Loop
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for(i=0; i<10; i++)
+        cout<<"Hello, World!\n";
+    cout<<endl;
+    return 0;
+}
