f47092fdd5e3004001ce6dcb3f5a847d1f184c4f
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-21 18:36:33 +0530

    Create Print Pattern of Alphabet Characters.cpp

---
+++ pattern_programs/Print Pattern of Alphabet Characters.cpp
@@ -0,0 +1,19 @@
+// C++ Print Pattern of Alphabet Characters
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    char ch='A';
+    for(i=0; i<6; i++)
+    {
+        for(j=0; j<=i; j++)
+        {
+            cout<<ch<<" ";
+        }
+        ch++;
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
