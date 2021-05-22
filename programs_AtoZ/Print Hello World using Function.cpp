54eaa4cd6ce90590df55b41df2add4def35d1a9c
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-22 21:00:34 +0530

    Create Print Hello World using Function.cpp

---
+++ programs_AtoZ/Print Hello World using Function.cpp
@@ -0,0 +1,14 @@
+// C++ Print Hello World using Function
+#include<iostream>
+using namespace std;
+void printHello();
+int main()
+{
+    printHello();
+    cout<<endl;
+    return 0;
+}
+void printHello()
+{
+    cout<<"Hello, World!";
+}
