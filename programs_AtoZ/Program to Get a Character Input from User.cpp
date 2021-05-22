8bfa25bf7ab64244627bd1d45c759ef70af9f149
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-22 21:44:18 +0530

    Create Program to Get a Character Input from User.cpp

---
+++ programs_AtoZ/Program to Get a Character Input from User.cpp
@@ -0,0 +1,12 @@
+// C++ Program to Get a Character Input from User
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    cout<<"Enter the Character: ";
+    cin>>ch;
+    cout<<"\nYou've entered: "<<ch;
+    cout<<endl;
+    return 0;
+}
