673473599848081ba126d958161c032dcabe6370
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-22 21:48:52 +0530

    Create Get String Input with Spaces using gets function.cpp

---
+++ programs_AtoZ/Get String Input with Spaces using gets function.cpp
@@ -0,0 +1,13 @@
+// C++ Get String Input with Spaces using gets function
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    char str[200];
+    cout<<"Enter the String: ";
+    gets(str);
+    cout<<"\nYou've entered: "<<str;
+    cout<<endl;
+    return 0;
+}
