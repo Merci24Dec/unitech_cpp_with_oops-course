2fbcafbf70b2240117fed20234ac731b47aa1ba8
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-22 21:46:14 +0530

    Create Program to Get String Input from User.cpp

---
+++ programs_AtoZ/Program to Get String Input from User.cpp
@@ -0,0 +1,12 @@
+// C++ Program to Get String Input from User
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str[200];
+    cout<<"Enter the String: ";
+    cin>>str;
+    cout<<"\nYou've entered: "<<str;
+    cout<<endl;
+    return 0;
+}
