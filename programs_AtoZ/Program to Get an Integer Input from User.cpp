d406e3d9533ea2cc0eeb6656467bcf735288e690
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-22 21:42:14 +0530

    Create Program to Get an Integer Input from User.cpp

---
+++ programs_AtoZ/Program to Get an Integer Input from User.cpp
@@ -0,0 +1,12 @@
+// C++ Program to Get an Integer Input from User
+#include<iostream>
+using namespace std;
+int main()
+{
+    int val;
+    cout<<"Enter the Number: ";
+    cin>>val;
+    cout<<"\nThe Value is "<<val;
+    cout<<endl;
+    return 0;
+}
