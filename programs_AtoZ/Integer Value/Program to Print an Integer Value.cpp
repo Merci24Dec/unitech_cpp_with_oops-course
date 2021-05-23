5a9f3030800b68c3d5aeaafddafc512c3d9f54e6
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-23 19:41:17 +0530

    Create Program to Print an Integer Value.cpp

---
+++ programs_AtoZ/Integer Value/Program to Print an Integer Value.cpp
@@ -0,0 +1,10 @@
+// C++ Program to Print an Integer Value
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num=10;
+    cout<<"The Value of 'num' is "<<num;
+    cout<<endl;
+    return 0;
+}
