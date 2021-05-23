5258ccba4a6a3ee2c6152f0bd1df6ab3c2fa9085
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-23 19:52:38 +0530

    Create Add two Numbers of Integer Type.cpp

---
+++ programs_AtoZ/Add 2 Numbers/Add two Numbers of Integer Type.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, add;
+    cout<<"Enter Two Numbers: ";
+    cin>>num1>>num2;
+    add = num1+num2;
+    cout<<"\nResult = "<<add;
+    cout<<endl;
+    return 0;
+}
