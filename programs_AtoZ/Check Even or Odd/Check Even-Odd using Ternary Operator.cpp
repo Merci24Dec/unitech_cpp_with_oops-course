de2e0153f51e374e14baa805e18951179070f2a5
Akash Dubey <akashdubey24122707@gmail.com>
2021-05-24 21:52:21 +0530

    Create Check Even-Odd using Ternary Operator.cpp

---
+++ programs_AtoZ/Check Even or Odd/Check Even-Odd using Ternary Operator.cpp
@@ -0,0 +1,12 @@
+// Check Even/Odd using Ternary Operator
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout<<"Enter a Number: ";
+    cin>>num;
+    (num%2==0)?(cout<<"\nEven Number"):(cout<<"\nOdd Number");
+    cout<<endl;
+    return 0;
+}v
