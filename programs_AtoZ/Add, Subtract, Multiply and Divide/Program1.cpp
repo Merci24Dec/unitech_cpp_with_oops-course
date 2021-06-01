59dfd3709c4d5871fc8305ff79f866baa6de67f8
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-01 19:07:13 +0530

    Create Program1.cpp

---
+++ programs_AtoZ/Add, Subtract, Multiply and Divide/Program1.cpp
@@ -0,0 +1,19 @@
+// C++ Program to Add, Subtract, Multiply and Divide
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2, res;
+    cout<<"Enter Two Numbers: ";
+    cin>>num1>>num2;
+    res = num1+num2;
+    cout<<endl<<"Addition Result = "<<res<<endl;
+    res = num1-num2;
+    cout<<endl<<"Subtraction Result = "<<res<<endl;
+    res = num1*num2;
+    cout<<endl<<"Multiplication Result = "<<res<<endl;
+    res = num1/num2;
+    cout<<endl<<"Division Result = "<<res<<endl;
+    return 0;
+}
