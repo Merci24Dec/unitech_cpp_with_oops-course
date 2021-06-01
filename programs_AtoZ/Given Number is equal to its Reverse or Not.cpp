e18cc537b4d5c049831878c2ac4e9b632c97a255
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-01 19:02:02 +0530

    Create Given Number is equal to its Reverse or Not.cpp

---
+++ programs_AtoZ/Given Number is equal to its Reverse or Not.cpp
@@ -0,0 +1,23 @@
+// C++ Program to check Given Number is equal to its Reverse or Not
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, orig, rev=0, rem;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    orig = num;
+    while(num>0)
+    {
+        rem = num%10;
+        rev = (rev*10)+rem;
+        num = num/10;
+    }
+    if(orig==rev)
+        cout<<"\nThis Number is equal to its Reverse";
+    else
+        cout<<"\nThis Number is not equal to its Reverse";
+    cout<<endl;
+    return 0;
+}
