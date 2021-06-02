b1eab9266f637f36ba8b80615d73c9e4b89a9c15
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-02 19:19:59 +0530

    Create using_while_Loop.cpp

---
+++ programs_AtoZ/Sum of Digits of a Number/using_while_Loop.cpp
@@ -0,0 +1,20 @@
+// Find Sum of Digits of a Number using while Loop
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, rem, sum=0;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    while(num>0)
+    {
+        rem = num%10;
+        sum = sum+rem;
+        num = num/10;
+    }
+    cout<<"\nSum of Digits = "<<sum;
+    cout<<endl;
+    return 0;
+}
