3e8ce38184438584a5659033fa32f0589ecb5766
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-02 19:35:11 +0530

    Create without_using_Loop.cpp

---
+++ programs_AtoZ/Sum of Digits of a Number/without_using_Loop.cpp
@@ -0,0 +1,19 @@
+// Find Sum of Digits of a Number without using Loop
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, rem, sum=0;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    CODESCRSCKER:
+        rem = num%10;
+        sum = sum+rem;
+        num = num/10;
+        if(num>0)
+            goto CODESCRSCKER;
+    cout<<"\nSum of Digits = "<<sum;
+    cout<<endl;
+    return 0;
+}
