a16396a0bbe35b46d3854cdc2c246bfeffef8e9a
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-04 20:17:14 +0530

    Create Simple.cpp

---
+++ programs_AtoZ/Find Average and Percentage Marks/Simple.cpp
@@ -0,0 +1,21 @@
+// C++ Program to Find Average and Percentage Marks
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, mark[5];
+    float sum=0, avg, perc;
+    cout<<"Enter Marks obtained in 5 Subjects: ";
+    for(i=0; i<5; i++)
+    {
+        cin>>mark[i];
+        sum = sum+mark[i];
+    }
+    avg = sum/5;
+    perc = (sum/500)*100;
+    cout<<"\nAverage Marks = "<<avg;
+    cout<<"\nPercentage Marks = "<<perc<<"%";
+    cout<<endl;
+    return 0;
+}
