cf8fa22832ef0ce8e7cdd66681ed1ab56cb4e0a4
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-05 21:14:00 +0530

    Create Method2.cpp

---
+++ programs_AtoZ/Multiplication Table of Number/Table within a Given Range/Method2.cpp
@@ -0,0 +1,42 @@
+// C++ Print Table in a Given Range
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int numOne, numTwo, num, i, j, res;
+    cout<<"Enter the Range: ";
+    cin>>numOne>>numTwo;
+    if(numTwo>numOne)
+    {
+        cout<<"--------Table from "<<numOne<<" to "<<numTwo<<"--------\n\n";
+        for(i=numOne; i<=numTwo; i++)
+        {
+            num = i;
+            cout<<"Table of "<<num<<": ";
+            for(j=1; j<=10; j++)
+            {
+                res = num*j;
+                cout<<res<<"  ";
+            }
+            cout<<endl;
+        }
+    }
+    else
+    {
+        cout<<"--------Table from "<<numTwo<<" to "<<numOne<<"--------\n\n";
+        for(i=numTwo; i<=numOne; i++)
+        {
+            num = i;
+            cout<<"Table of "<<num<<": ";
+            for(j=1; j<=10; j++)
+            {
+                res = num*j;
+                cout<<res<<"  ";
+            }
+            cout<<endl;
+        }
+    }
+    cout<<endl;
+    return 0;
+}
