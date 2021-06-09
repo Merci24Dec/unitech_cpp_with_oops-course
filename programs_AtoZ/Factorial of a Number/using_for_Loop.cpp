17802c50577d24ad0a1c4b44309b57ed78f4b969
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-09 20:43:00 +0530

    Create using_for_Loop.cpp

---
+++ programs_AtoZ/Factorial of a Number/using_for_Loop.cpp
@@ -0,0 +1,15 @@
+// C++ Program to Find Factorial of a Number
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num, i, fact=1;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    for(i=num; i>=1; i--)
+        fact = fact*i;
+    cout<<"\nFactorial = "<<fact;
+    cout<<endl;
+    return 0;
+}
