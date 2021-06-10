43bc3806570888ab3e3b2e81014824140923759e
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-10 23:10:13 +0530

    Create Area_of_Square.cpp

---
+++ programs_AtoZ/Area of Square/Area, Perimeter of Square/Area_of_Square.cpp
@@ -0,0 +1,14 @@
+// C++ Program to Find Area of Square
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float len, area;
+    cout<<"Enter the Length of Square: ";
+    cin>>len;
+    area = len*len;
+    cout<<"\nArea = "<<area;
+    cout<<endl;
+    return 0;
+}
