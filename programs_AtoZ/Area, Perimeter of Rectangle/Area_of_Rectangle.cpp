2160c8969986a0ce3b85339ccf69f9ec2b28d9fe
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-11 23:13:35 +0530

    Create Area_of_Rectangle.cpp

---
+++ programs_AtoZ/Area, Perimeter of Rectangle/Area_of_Rectangle.cpp
@@ -0,0 +1,16 @@
+// C++ Program to Find Area of Rectangle
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float len, bre, area;
+    cout<<"Enter Length of Rectangle: ";
+    cin>>len;
+    cout<<"Enter Breadth of Rectangle: ";
+    cin>>bre;
+    area = len*bre;
+    cout<<"\nArea = "<<area;
+    cout<<endl;
+    return 0;
+}
