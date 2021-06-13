efdfd8fb3c6db7e9c2dc91a24e1b13d8d4b25b4d
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-13 18:54:53 +0530

    Create Area_of_a_Circle.cpp

---
+++ programs_AtoZ/Area, Circumference of Circle/Area_of_a_Circle.cpp
@@ -0,0 +1,14 @@
+// C++ Program to Find Area of a Circle
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float rad, area;
+    cout<<"Enter the Radius of Circle: ";
+    cin>>rad;
+    area = 3.14*rad*rad;
+    cout<<"\nArea of Circle = "<<area;
+    cout<<endl;
+    return 0;
+}
