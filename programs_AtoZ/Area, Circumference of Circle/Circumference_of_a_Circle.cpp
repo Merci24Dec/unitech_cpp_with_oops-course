ee1d8c2b0e42499a2cb66ee5e4b8779171b06bfd
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-13 20:41:45 +0530

    Create Circumference_of_a_Circle.cpp

---
+++ programs_AtoZ/Area, Circumference of Circle/Circumference_of_a_Circle.cpp
@@ -0,0 +1,14 @@
+// C++ Program to Find Circumference of a Circle
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float rad, circum;
+    cout<<"Enter the Radius of Circle: ";
+    cin>>rad;
+    circum = 2*3.14*rad;
+    cout<<"\nCircumference of Circle = "<<circum;
+    cout<<endl;
+    return 0;
+}
