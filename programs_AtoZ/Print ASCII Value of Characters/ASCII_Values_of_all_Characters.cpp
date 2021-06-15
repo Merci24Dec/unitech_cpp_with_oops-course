e23d6c9baa690a58c027eb466f9a7fb94d9225e9
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-15 20:39:52 +0530

    Create ASCII_Values_of_all_Characters.cpp

---
+++ programs_AtoZ/Print ASCII Value of Characters/ASCII_Values_of_all_Characters.cpp
@@ -0,0 +1,17 @@
+// C++ Print ASCII Values of all Characters
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    int i;
+    cout<<"Character\t\tASCII Value\n";
+    for(i=0; i<255; i++)
+    {
+        ch = i;
+        cout<<ch<<"\t\t\t"<<i<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
