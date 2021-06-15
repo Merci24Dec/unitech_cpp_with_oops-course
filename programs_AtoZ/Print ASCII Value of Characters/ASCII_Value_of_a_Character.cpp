c6e73f329ba5b5906caca8aaff445fcd651c649f
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-15 20:35:54 +0530

    Create ASCII_Value_of_a_Character.cpp

---
+++ programs_AtoZ/Print ASCII Value of Characters/ASCII_Value_of_a_Character.cpp
@@ -0,0 +1,15 @@
+// C++ Program to Print ASCII Value of a Character
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    int i;
+    cout<<"Enter the Character: ";
+    cin>>ch;
+    i = ch;
+    cout<<"\nASCII Value = "<<i;
+    cout<<endl;
+    return 0;
+}
