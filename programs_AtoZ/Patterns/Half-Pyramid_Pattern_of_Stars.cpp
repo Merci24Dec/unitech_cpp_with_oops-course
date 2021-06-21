434b472aec92e17eb9bf2e0b046f22ed08da8504
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-21 22:42:23 +0530

    Create Half-Pyramid_Pattern_of_Stars.cpp

---
+++ programs_AtoZ/Patterns/Half-Pyramid_Pattern_of_Stars.cpp
@@ -0,0 +1,16 @@
+// C++ Print Half-Pyramid Pattern of Stars
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j;
+    for(i=0; i<6; i++)
+    {
+        for(j=0; j<=i; j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
