f08bd46c45ef46d1b3b67d08abd055796ffe4272
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-24 21:52:17 +0530

    Create Full_Pyramid_Pattern_of_Stars2.cpp

---
+++ programs_AtoZ/Patterns/Full_Pyramid_Pattern_of_Stars2.cpp
@@ -0,0 +1,18 @@
+// C++ Print Full Pyramid Pattern of Stars
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, space, j;
+    for(i=1; i<=6; i++)
+    {
+        for(space=6; space>i; space--)
+            cout<<" ";
+        for(j=0; j<i; j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
