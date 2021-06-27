ac1a55c3aa1a3f290b73d657a5e1175695ab8954
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-27 22:42:38 +0530

    Create Triangle_Star_Pattern.cpp

---
+++ programs_AtoZ/Patterns/Triangle_Star_Pattern.cpp
@@ -0,0 +1,19 @@
+// C++ Print Triangle Star Pattern
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j, space=10;
+    for(i=0; i<6; i++)
+    {
+        for(j=0; j<space; j++)
+            cout<<" ";
+        space = space-2;
+        for(j=0; j<=i; j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
