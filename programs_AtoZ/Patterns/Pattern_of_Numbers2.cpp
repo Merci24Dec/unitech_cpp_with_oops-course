87c74e9b53791bc670367fd64f2030b4df0491ac
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-28 23:10:53 +0530

    Create Pattern_of_Numbers2.cpp

---
+++ programs_AtoZ/Patterns/Pattern_of_Numbers2.cpp
@@ -0,0 +1,20 @@
+// C++ Print Pattern of Numbers
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j, num;
+    for(i=0; i<6; i++)
+    {
+        num=1;
+        for(j=0; j<=i; j++)
+        {
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
