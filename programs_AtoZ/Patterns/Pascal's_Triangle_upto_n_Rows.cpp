1b03ce65f6d0f4ede8278581e3afb3f932f251a2
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-30 18:50:20 +0530

    Create Pascal's_Triangle_upto_n_Rows.cpp

---
+++ programs_AtoZ/Patterns/Pascal's_Triangle_upto_n_Rows.cpp
@@ -0,0 +1,28 @@
+// C++ Print Pascal's Triangle upto n Rows
+
+#include<iostream>
+using namespace std;
+long int fact(int);
+int main()
+{
+    int i, c, rowSize;
+    cout<<"Enter the Number of Rows: ";
+    cin>>rowSize;
+    for(i=0; i<rowSize; i++)
+    {
+        for(c=(rowSize-1); c>i; c--)
+            cout<<" ";
+        for(c=0; c<=i; c++)
+            cout<<fact(i)/(fact(c)*fact(i-c))<<" ";
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
+long int fact(int n)
+{
+    int i, factRes=1;
+    for(i=1; i<=n; i++)
+        factRes = factRes*i;
+    return factRes;
+}
