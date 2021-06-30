d661bf48b0f56f2ccf25110d5625be12b07132a3
Akash Dubey <akashdubey24122707@gmail.com>
2021-06-30 17:49:53 +0530

    Create Floyd's_Triangle.cpp

---
+++ programs_AtoZ/Patterns/Floyd's_Triangle.cpp
@@ -0,0 +1,18 @@
+// C++ Program to Print Floyd's Triangle
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i, j, num=1;
+    for(i=0; i<5; i++)
+    {
+        for(j=0; j<=i; j++)
+        {
+            cout<<num<<" ";
+            num++;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
