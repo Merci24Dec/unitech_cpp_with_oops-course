adc20568abba5fa58c1459b289031237295deedd
Akash Dubey <akashdubey24122707@gmail.com>
2021-07-17 19:30:04 +0530

    Create Insert_Element_at_End_of_Array.cpp

---
+++ programs_AtoZ/Insert an Element in an Array/Insert_Element_at_End_of_Array.cpp
@@ -0,0 +1,19 @@
+// C++ Program to Insert Element at End of Array
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[6], i, elem;
+    cout<<"Enter 5 Array Elements: ";
+    for(i=0; i<5; i++)
+        cin>>arr[i];
+    cout<<"\nEnter Element to Insert: ";
+    cin>>elem;
+    arr[i] = elem;
+    cout<<"\nThe New Array is:\n";
+    for(i=0; i<6; i++)
+        cout<<arr[i]<<"  ";
+    cout<<endl;
+    return 0;
+}
