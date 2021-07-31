61dbf8de28640bebc6bef63061df9c9253b3f2c2
Akash Dubey <akashdubey24122707@gmail.com>
2021-07-31 23:11:17 +0530

    Create Compare_Two_Strings_without_strcmp_function.cpp

---
+++ programs_AtoZ/Comparing Strings/Compare_Two_Strings_without_strcmp_function.cpp
@@ -0,0 +1,28 @@
+// C++ Program to Compare Two Strings without strcmp()
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str1[50], str2[50];
+    int i=0, chk=0;
+    cout<<"Enter the First String: ";
+    cin>>str1;
+    cout<<"Enter the Second String: ";
+    cin>>str2;
+    while(str1[i]!='\0' || str2[i]!='\0')
+    {
+        if(str1[i]!=str2[i])
+        {
+            chk = 1;
+            break;
+        }
+        i++;
+    }
+    if(chk==0)
+        cout<<"\nStrings are Equal";
+    else
+        cout<<"\nStrings are not Equal";
+    cout<<endl;
+    return 0;
+}
