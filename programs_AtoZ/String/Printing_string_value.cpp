69689fc1740fe446e2bef03651d2dd4ea72f183a
Akash Dubey <akashdubey24122707@gmail.com>
2021-07-31 22:54:00 +0530

    Create Printing_string_value.cpp

---
+++ programs_AtoZ/String/Printing_string_value.cpp
@@ -0,0 +1,13 @@
+// C++ Program to Print String
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    char str[20];
+    cout<<"Enter Your First Name: ";
+    cin>>str;
+    cout<<"\nHello, "<<str;
+    cout<<endl;
+    return 0;
+}
