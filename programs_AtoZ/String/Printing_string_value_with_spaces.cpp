68d66f562213ece0ac99d22a1669a202f4ee8346
Akash Dubey <akashdubey24122707@gmail.com>
2021-07-31 22:55:50 +0530

    Create Printing_string_value_with_spaces.cpp

---
+++ programs_AtoZ/String/Printing_string_value_with_spaces.cpp
@@ -0,0 +1,14 @@
+// C++ Program to Print String
+
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    char str[200];
+    cout<<"Enter the String: ";
+    gets(str);
+    cout<<"\nYou've entered: "<<str;
+    cout<<endl;
+    return 0;
+}
